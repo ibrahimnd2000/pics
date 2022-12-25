e30e736a755a556a4dc6109576551b17e39ce28f
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-25 14:40:04 +0500

    Added grid styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './ImageList.css';
 
 const ImageList = (props) => {
   const images = props.images.map(({ id, urls, alt_description }) => {
     return <img key={id} src={urls.regular} alt={alt_description} />;
   });
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
