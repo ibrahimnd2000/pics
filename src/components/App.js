626ea6a7dbaef1f032fe673143a2df16a1babbdf
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-25 11:33:44 +0500

    Added state to the API implementation

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,27 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get('/search/photos', {
-        baseURL: 'https://api.unsplash.com',
-        params: {
-          query: term,
-        },
-        headers: {
-          Authorization:
-            'Client-ID fV4a_IfzalbLDjtMGQBZXJbU-OIP5eSp9EkJJVbB2G8',
-        },
-      })
-      .then((response) => {});
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('/search/photos', {
+      baseURL: 'https://api.unsplash.com',
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: 'Client-ID fV4a_IfzalbLDjtMGQBZXJbU-OIP5eSp9EkJJVbB2G8',
+      },
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
