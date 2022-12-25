d65ef90cc65507d22e92258454beafa8a96cfdd0
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-25 14:53:20 +0500

    Destructured props

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 class ImageCard extends React.Component {
   render() {
+    const { urls, alt_description } = this.props.image;
+
     return (
       <div>
-        <img
-          src={this.props.image.urls.regular}
-          alt={this.props.image.alt_description}
-        />
+        <img src={urls.regular} alt={alt_description} />
       </div>
     );
   }
